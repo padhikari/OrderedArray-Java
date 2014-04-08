4335b39ed1c0a4027b62c793e62f884a2642b7bc
pabitraadhikari@gmail.com <pabitraadhikari@gmail.com>
2014-04-08 14:55:18 -0700

    create class for Ordered array

---
+++ OrderedMain.java
@@ -0,0 +1,15 @@
+class OrderedArray {
+	
+	private int[] array; //array definition
+	private int numOfElem; //total num of elements
+	
+	public OrderedArray(int max){ //constructor
+		array= new int[max];
+	}
+}
+
+class OrderedMain {
+	public static void main(String[] args) {
+        
+    }
+}
